ab18c93a4acb83f227a82fe7aa022dc13e1c16b3
omar01411 <o.taha@glcpaints.com>
2026-09-23 10:18:46 +0000

    Deploying to main from @ GlcPaintsDeveloper/glcsila@b4d6ab1c1e4e56129f07e23d905e144af9b359f5 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3613935442" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "304168272" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
